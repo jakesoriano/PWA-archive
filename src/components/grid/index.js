59ababa0e0b019b3871c1afe3e36211d90fcdc5e
DESKTOP-9M8Q580\PC <alvinjavelosa@gmail.com>
2021-12-05 18:50:34 +0800

    landingPage - signInUp button and LeniLogow

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -1,5 +1,6 @@
 export { default as ModalButtons } from './ModalButtons';
 export { default as AccountProfile } from './AccountProfile';
 export { default as PageTitle } from './PageTitle';
+export { default as LandingPage } from './LandingPage';
 export { default as TermsConditions } from './TermsConditions';
 export { default as NewsAndEvents } from './NewsAndEvents';
