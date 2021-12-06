7f4c3340173b48436bccf12315faff46ba259833
Jomar <jomar.ocampo@mnltechnology.com>
2021-12-07 00:11:50 +0800

    remove unse code

--- src/components/grid/Invite/index.js
+++ src/components/grid/Invite/index.js
@@ -19,26 +19,6 @@ class Invite extends Component {
 
 	  return (
 			<>
-				<Form>
-					<Form.Group controlId="formBasicEmail">
-						<Form.Label>Email address</Form.Label>
-						<Form.Control type="email" placeholder="Enter email" />
-						<Form.Text className="text-muted">
-							We'll never share your email with anyone else.
-							</Form.Text>
-					</Form.Group>
-
-					<Form.Group controlId="formBasicPassword">
-						<Form.Label>Password</Form.Label>
-						<Form.Control type="password" placeholder="Password" />
-					</Form.Group>
-					<Form.Group controlId="formBasicChecbox">
-						<Form.Check type="checkbox" label="Check me out" />
-					</Form.Group>
-					<Button variant="primary" type="submit">
-						Submit
-						</Button>
-				</Form>
 				<div className={style.invitedWrap}>
 					<div className={`${style.item} ${style.header}`}>
 						<p className={`bold ${style.name}`}>{getTranslation('ADDED_MEMBERS')}</p>
