4da404e07600e301a403ffe89490d1b461eb364e
Jomar <jomar.ocampo@mnltechnology.com>
2022-02-04 14:46:02 +0800

    update selfie, same with home > avatar > direct to front camera

--- src/components/grid/RegistrationInvite/index.js
+++ src/components/grid/RegistrationInvite/index.js
@@ -3,8 +3,20 @@ import { route } from 'preact-router';
 import { updateAvatar } from '_mutations';
 import { connect } from 'unistore/preact';
 import { nativeSelfie } from '_platform/helpers';
-import { getTranslation, showAlertBox } from '_helpers';
-import { InviteForm, ImageLoader, ButtonDescription } from '_components/core';
+import {
+	getTranslation,
+	showAlertBox,
+	promptModal,
+  resizeImage,
+  uploadFile,
+	displayPageLoader
+} from '_helpers';
+import {
+	InviteForm,
+	ImageLoader,
+	ButtonDescription,
+	FormInput
+} from '_components/core';
 // import { nativeShare } from '_platform/helpers';
 // eslint-disable-next-line import/extensions
 import style from './style';
@@ -15,38 +27,83 @@ class RegistrationInvite extends Component {
 		super(props);
 		this.state = {
 			hasSentInvite: false,
+			attachment: {
+				file: null
+			}
 		};
 	}
 
-	onDownloadKit = (url) => {
-		window.open(url, '_blank');
+	componentDidMount = () => {
+		promptModal({
+			title: getTranslation('SELFIE_TITLE'),
+			message: getTranslation('SELFIE_MSG'),
+			textNo: getTranslation('SKIP'),
+			textYes: getTranslation('OPEN_CAMERA'),
+			cbOk: () => {
+				document.getElementById('inputAttachment').click();
+				promptModal(null);
+			}
+		});
 	};
 
-	componentDidMount = () => {
-		if (this.props.route && this.props.route.previous &&
-			this.props.route.previous.indexOf('registration-otp') > -1) {
-			nativeSelfie().then((image) => {
-				if (image) {
-					let data = {
-						image: image
-					}
-					updateAvatar(data).then((res) => {
-						if (!res.success) {
-							showAlertBox({
-								message: getTranslation('SOMETHING_WRONG')
-							});
-						}
-					})
-				}
-			});
-		}
+  updateUserImage = (file) => {
+		this.setState({
+			attachment: {
+				file: file,
+			}
+		});
+    displayPageLoader(true);
+    uploadFile({file: file}).then((res) => {
+      displayPageLoader(false);
+      if (res.success && res.data) {
+        updateAvatar(res.data).then((resUpdate) => {
+          if (resUpdate.success) {
+            showAlertBox({
+              message: 'UPDATE_AVATAR_SUCCESS',
+              success: true
+            });
+          } else {
+            showAlertBox({
+              message: (resUpdate.errMessage || 'SOMETHING_WRONG')
+            });
+          }
+        })
+      } else {
+        showAlertBox({
+          message: (res.errMessage || 'SOMETHING_WRONG')
+        });
+      }
+    })
+  }
+
+	onAttachmentChange = (file) => {
+    if (!file) {
+      return;
+    }
+
+    resizeImage({
+      file: file,
+      maxSize: 1200
+    })
+    .then(resizedImage => {
+      console.log("upload resized image", resizedImage);
+      this.updateUserImage(resizedImage);
+    })
+    .catch(err => {
+      // console.error(err);
+      this.updateUserImage(file);
+    });
+	};
+
+	onDownloadKit = (url) => {
+		window.open(url, '_blank');
 	};
 
 	handleContinue = () => {
 		route('home', true);
 	};
 
-	render = ({ authUser, invited }, { hasSentInvite }) => {
+	render = ({ authUser, invited }, { hasSentInvite, attachment }) => {
 		if (!authUser) {
 			return null;
 		}
@@ -54,6 +111,22 @@ class RegistrationInvite extends Component {
 		return (
 			<div className={style.inviteWrap}>
 				<div className={style.inviteForm}>
+					{/* Hidden input */}
+					<FormInput
+							id='inputAttachment'
+							className={style.attachment}
+							style={{error: style.attachment}}
+							value={attachment.file}
+							type="file"
+							accept="image/*"
+							capture="user"
+							onBlur={(e) => {
+								this.onAttachmentChange(e.target.files[0])
+							}}
+							onInput={(e) => {
+								this.onAttachmentChange(e.target.files[0])
+							}}
+						/>
 					<p className={`${style.heading} extraBold`}>
 						{getTranslation('SET_GOAL')}
 					</p>
