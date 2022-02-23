4314cf6ef9878e5a40fdb4f019f379de7cc46e77
lance.malaga <lance.malaga@whitecloak.com>
2022-02-23 15:26:50 +0800

    update landing page

---
+++ src/components/grid/CommunitiesAndKit/index.js
@@ -0,0 +1,120 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import { Component } from 'preact';
+import { connect } from 'unistore/preact';
+import { getTranslation } from '_helpers';
+import {
+  ImageLoader,
+	ButtonDescription,
+	InitialSignup,
+} from '_components/core';
+// eslint-disable-next-line import/extensions
+import style from './style';
+import { fetchAppLandingConfig } from '../../../mutations/appLandingConfig';
+
+// carousel
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+// eslint-disable-next-line react/prefer-stateless-function
+class CommunitiesAndKit extends Component {
+  constructor(props) {
+		super(props);
+		this.state = {
+      showInitialSignupForm: false,
+		};
+	}
+
+  componentDidMount = () => {
+		fetchAppLandingConfig();
+	};
+
+  toggleSignupForm = () => {
+		const { showInitialSignupForm } = this.state;
+		this.setState({ showInitialSignupForm: !showInitialSignupForm });
+	};
+
+	render = ({ toggleCommunitiesAndKit, isOpen, appLandingConfig }) => {
+    const data = appLandingConfig.data;
+	  return (
+	    <div className={style.contentContainer}>
+	      {/* eslint-disable-next-line react/self-closing-comp */}
+	      <div
+	        onClick={toggleCommunitiesAndKit}
+	        className={isOpen ? style.contentOutside : null}
+	      >
+	      </div>
+	      <div
+	        className={isOpen ? `${style.comsAndKit} ${style.toggled}` : style.comsAndKit}
+	      >
+	        {/* Communities and Kit Content Starts Here */}
+          <div className={style.communities}>
+            <p className={`extraBold ${style.title}`}> {getTranslation('COMMUNITIES')} </p>
+            <div className={style.community}>
+              <ImageLoader
+                src={data.event1.image}
+                style={{ container: style.landingImg }}
+                lazy
+              />
+              <div className={style.communityContent}>
+                <h4 className={`extraBold`}> {data.event1.title} </h4>
+                <p className={`bold`}> {data.event1.date ? data.event1.date : null} </p>
+                <p> {data.event1.body}{data.event1.body.length > 98 ? "..." : ""} </p>
+              </div>
+            </div>
+            <div className={style.community}>
+              <ImageLoader
+                src={data.event2.image}
+                style={{ container: style.landingImg }}
+                lazy
+              />
+              <div className={style.communityContent}>
+                <h4 className={`extraBold`}> {data.event2.title} </h4>
+                <p className={`bold`}> {data.event2.date ? data.event2.date : null} </p>
+                <p> {data.event2.body}{data.event2.body.length > 98 ? "..." : ""} </p>
+              </div>
+            </div>
+            <div>
+              <p className={`extraBold ${style.title}`}> {getTranslation('KAKAMPINK_KIT')} </p>
+              <Carousel
+                showArrows={false}
+                showStatus={false}
+                showThumbs={false}
+              >
+                <div>
+                  <ImageLoader
+                    src="https://mobile-kkp.s3.ap-southeast-1.amazonaws.com/uploads/01FWD1CF8FEVNF5YG0ZFJWJF8H.png"
+                    style={{ container: style.kitImage }}
+                    lazy
+                  />
+                </div>
+                <div>
+                  <ImageLoader
+                    src="https://mobile-kkp.s3.ap-southeast-1.amazonaws.com/uploads/01FWD1D4Z3RYF7M7H864873N7J.png"
+                    style={{ container: style.kitImage }}
+                    lazy
+                  />
+                </div>
+              </Carousel>
+            </div>
+            <div className={style.signUpBtn}>
+              <ButtonDescription
+                onClickCallback={this.toggleSignupForm}   
+                text={getTranslation('SIGNUP_BUTTON')}
+                bottomDescription=""
+              />
+            </div>
+          </div>
+	      </div>
+
+        <InitialSignup
+					isOpen={this.state.showInitialSignupForm}
+					toggleSignupForm={this.toggleSignupForm}
+					parent={this.props.parent}
+				/>
+	    </div>
+	  );
+	};
+}
+
+export default connect(['appLandingConfig', 'authUser'])(CommunitiesAndKit);
--- src/components/grid/LandingPage/index.js
+++ src/components/grid/LandingPage/index.js
@@ -1,5 +1,7 @@
 import { Component } from 'preact';
+import { route } from 'preact-router';
 import { connect } from 'unistore/preact';
+
 import {
 	ImageLoader,
 	ButtonDescription,
@@ -7,6 +9,8 @@ import {
 	Login,
 } from '_components/core';
 import { getTranslation } from '_helpers';
+import { fetchAppLandingConfig } from '../../../mutations/appLandingConfig';
+import CommunitiesAndKit from '../CommunitiesAndKit';
 import supportIcon from '../../../assets/icons/support_icon.png';
 import style from './style';
 
@@ -16,9 +20,14 @@ class LandingPage extends Component {
 		super(props);
 		this.state = {
 			showLoginForm: false,
-      showInitialSignupForm: false
+      showInitialSignupForm: false,
+      showCommunitiesAndKit: false,
 		};
 	}
+
+  componentDidMount = () => {
+		fetchAppLandingConfig();
+	};
 
 	toggleLoginForm = () => {
 		const { showLoginForm } = this.state;
@@ -29,19 +38,64 @@ class LandingPage extends Component {
 		const { showInitialSignupForm } = this.state;
 		this.setState({ showInitialSignupForm: !showInitialSignupForm });
 	};
+
+	toggleCommunitiesAndKit = () => {
+		const { showCommunitiesAndKit } = this.state;
+		this.setState({ showCommunitiesAndKit: !showCommunitiesAndKit });
+	};
   
-  render = () => {
+  showPrivacyPolicy = () => {
+		route(`/${this.props.parent}/landing-data-privacy`);
+	};
+
+	showKayaNatin = () => {
+		route(`/${this.props.parent}/about-kaya-natin`);
+	};
+
+  render = ({ appLandingConfig }) => {
+    const data = appLandingConfig.data;
 		return (
 			<div className={style.landingPage}>
 				<div>
 					<div className={style.contentWrap}>
-						<div className={style.leniLogo}>
-							<div className={style.user}>
+						<div className={style.headline}>
+							<div className={style.announcements}>
 								<ImageLoader
-									src="assets/images/appLogo.png"
+									src={data.headline1.image}
 									style={{ container: style.landingImg }}
 									lazy
 								/>
+                <div className={style.announcementsContent}>
+                  <p> {getTranslation('HEADLINE')} </p>
+                  <h4 className={`extraBold`}> {data.headline1.title} </h4>
+                  <p className={`bold`}> {data.headline1.date ? data.headline1.date : null} </p>
+                  <p> {data.headline1.body}{data.headline1.body.length > 98 ? "..." : ""} </p>
+                </div>
+              </div>
+              <div className={style.heroPoints}>
+                <ImageLoader
+									src={data.headline2.image}
+									style={{ container: style.heroPointsTotal }}
+									lazy
+								/>
+                <div className={style.heroPointsContent}>
+                  <p className={`bold`}> {getTranslation('HERO_MSG')} </p>
+                  <h4 className={`bold`}> {data.headline2.title} </h4>
+                </div>
+              </div>
+            </div>
+            <div className={style.partners}>
+              <p className={`extraBold`}> {getTranslation('TRUSTED_BY')} </p>
+              <div className={`${style.partnerImages} ${data.trusted_by.length > 3 ? style.partnerImagesScroll : ""}`}>
+                {data.trusted_by.map(function(image, i){
+                  return (
+                    <img
+                      src={image}
+                      alt=""
+                      className={ style.landingImg }
+                    />
+                  )}
+                )}
 							</div>
 						</div>
 						<div className={style.signInUpBtnWrap}>
@@ -50,21 +104,33 @@ class LandingPage extends Component {
 								<a
 									className={`extraBold ${style.pClose}`}
 									onClick={this.toggleLoginForm}
-								>{getTranslation('SIGNIN')}</a>
+								>
+									{getTranslation('SIGNIN')}
+								</a>
 							</p>
-							<ButtonDescription
-								onClickCallback={this.toggleSignupForm}		
-								text={getTranslation('SIGNUP_BUTTON')}
-								bottomDescription=""
-							/>
-              <a
-                className={style.supportIcon}
-                href="https://m.me/leni2022app"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <img src={supportIcon} alt="support icon" />
-              </a>
+              <ButtonDescription
+                onClickCallback={this.toggleSignupForm}   
+                text={getTranslation('SIGNUP_BUTTON')}
+                bottomDescription=""
+              />
+              <ButtonDescription
+                onClickCallback={this.toggleCommunitiesAndKit}   
+                text={getTranslation('COMMUNITIES_AND_KIT')}
+                buttonStyle={`${style.buttonStyle}`}
+                bottomDescription=""
+              />
+              <div className={style.privacyPolicy}>
+                <a onClick={() => { this.showPrivacyPolicy() }}> {getTranslation('PRIVACY_POLICY')} </a> |
+                <a onClick={() => { this.showKayaNatin() }}> {getTranslation('KAYA_NATIN')} </a>
+                <a
+									className={style.supportIcon}
+									href="https://m.me/leni2022app"
+									target="_blank"
+									rel="noreferrer"
+								>
+									<img src={supportIcon} alt="support icon" />
+								</a>
+              </div>
 						</div>
 					</div>
 				</div>
@@ -72,14 +138,19 @@ class LandingPage extends Component {
 					isOpen={this.state.showLoginForm}
 					toggleLoginForm={this.toggleLoginForm}
 				/>
-			
+
 				<InitialSignup
 					isOpen={this.state.showInitialSignupForm}
 					toggleSignupForm={this.toggleSignupForm}
 					parent={this.props.parent}
 				/>
+
+        <CommunitiesAndKit
+          isOpen={this.state.showCommunitiesAndKit}
+          toggleCommunitiesAndKit={this.toggleCommunitiesAndKit}
+        />
       </div>
     );
   }
 }
-export default connect(['authUser'])(LandingPage);
+export default connect(['appLandingConfig', 'authUser'])(LandingPage);
