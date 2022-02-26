42aae2bb1c5380e29ff0734bd004c9c4d517512f
Lance Liam De Padua <33190396+lanceliamll@users.noreply.github.com>
2022-02-26 14:32:45 +0800

    Initial for New Community Volunteer Page (#49)

--- src/components/grid/index.js
+++ src/components/grid/index.js
@@ -46,3 +46,4 @@ export { default as LeniPedia } from './LeniPedia';
 export { default as CommunityManagement } from './CommunityManagement';
 export { default as OtherEvents } from './OtherEvents';
 export { default as EditProfile } from './EditProfile';
+export { default as CommunityVolunteer } from './CommunityVolunteer';
