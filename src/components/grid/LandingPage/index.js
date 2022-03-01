1403271f0c51913d4606a2fe83632a2279c74658
lance.malaga <lance.malaga@whitecloak.com>
2022-03-01 10:44:30 +0800

    update landing page ui

--- src/components/grid/LandingPage/index.js
+++ src/components/grid/LandingPage/index.js
@@ -65,91 +65,89 @@ class LandingPage extends Component {
 									style={{ container: style.landingImg }}
 									lazy
 								/>
-                <div className={style.announcementsContent}>
-                  <p> {getTranslation('HEADLINE')} </p>
-                  <h4 className={`extraBold`}> {data.headline1.title} </h4>
-                  <p className={`bold`}> {data.headline1.date ? data.headline1.date : null} </p>
-                  <p> {data.headline1.body}{data.headline1.body.length > 98 ? "..." : ""} </p>
-                </div>
-              </div>
-              <div className={style.heroPoints}>
-                <ImageLoader
+								<div className={style.announcementsContent}>
+									<p className={`bold`}> {getTranslation('HEADLINE')} </p>
+									<h4 className={`extraBold`}> {data.headline1.title} </h4>
+									<p className={`bold ${style.date}`}> {data.headline1.date ? data.headline1.date : null} </p>
+									<p> {data.headline1.body}{data.headline1.body.length > 98 ? "..." : ""} </p>
+								</div>
+							</div>
+							<div className={style.heroPoints}>
+								<ImageLoader
 									src={data.headline2.image}
 									style={{ container: style.heroPointsTotal }}
 									lazy
 								/>
-                <div className={style.heroPointsContent}>
-                  <p className={`bold`}> {getTranslation('HERO_MSG')} </p>
-                  <h4 className={`bold`}> {data.headline2.title} </h4>
-                </div>
-              </div>
-            </div>
-            <div className={style.partners}>
-              <p className={`extraBold`}> {getTranslation('TRUSTED_BY')} </p>
-              <div className={`${style.partnerImages} ${data.trusted_by.length > 3 ? style.partnerImagesScroll : ""}`}>
-                {data.trusted_by.map(function(image, i){
-                  return (
-                    <img
-                      src={image}
-                      alt=""
-                      className={ style.landingImg }
-                    />
-                  )}
-                )}
+								<div className={style.heroPointsContent}>
+								<p className={`bold`}> {getTranslation('HERO_MSG')} </p>
+								<h4 className={`extraBold`}> {data.headline2.title} </h4>
 							</div>
 						</div>
-						<div className={style.signInUpBtnWrap}>
-							<p>
-								{getTranslation('SIGNIN_DESC')}
-								<a
-									className={`extraBold ${style.pClose}`}
-									onClick={this.toggleLoginForm}
-								>
-									{getTranslation('SIGNIN')}
-								</a>
-							</p>
-              <ButtonDescription
-                onClickCallback={this.toggleSignupForm}   
-                text={getTranslation('SIGNUP_BUTTON')}
-                bottomDescription=""
-              />
-              <ButtonDescription
-                onClickCallback={this.toggleCommunitiesAndKit}   
-                text={getTranslation('COMMUNITIES_AND_KIT')}
-                buttonStyle={`${style.buttonStyle}`}
-                bottomDescription=""
-              />
-              <div className={style.privacyPolicy}>
-                <a onClick={() => { this.showPrivacyPolicy() }}> {getTranslation('PRIVACY_POLICY')} </a> |
-                <a onClick={() => { this.showKayaNatin() }}> {getTranslation('KAYA_NATIN')} </a>
-                <a
-									className={style.supportIcon}
-									href="https://m.me/leni2022app"
-									target="_blank"
-									rel="noreferrer"
-								>
-									<img src={supportIcon} alt="support icon" />
-								</a>
-              </div>
+					</div>
+					<div className={style.partners}>
+						<p> {getTranslation('TRUSTED_BY')} </p>
+						<div className={`${style.partnerImages} ${data.trusted_by.length > 3 ? style.partnerImagesScroll : ""}`}>
+							{data.trusted_by.map(function(image, i){
+								return (
+									<img
+									src={image}
+									alt=""
+									className={ style.landingImg }
+									/>
+								)}
+							)}
+						</div>
+					</div>
+					<div className={style.signInUpBtnWrap}>
+						<p>
+							{getTranslation('SIGNIN_DESC')}
+							<a onClick={this.toggleLoginForm}>
+								{getTranslation('SIGNIN')}
+							</a>
+						</p>
+						<ButtonDescription
+							onClickCallback={this.toggleSignupForm}   
+							text={getTranslation('SIGNUP_BUTTON')}
+							buttonStyle={`${style.buttonStyle}`}
+							bottomDescription=""
+						/>
+						<ButtonDescription
+							onClickCallback={this.toggleCommunitiesAndKit}   
+							text={getTranslation('COMMUNITIES_AND_KIT')}
+							buttonStyle={`${style.buttonStyle}`}
+							bottomDescription=""
+						/>
+						<div className={style.privacyPolicy}>
+							<a onClick={() => { this.showPrivacyPolicy() }}> {getTranslation('PRIVACY_POLICY')} </a> |
+							<a onClick={() => { this.showKayaNatin() }}> {getTranslation('KAYA_NATIN')} </a>
+							<a
+								className={style.supportIcon}
+								href="https://m.me/leni2022app"
+								target="_blank"
+								rel="noreferrer"
+							>
+								<img src={supportIcon} alt="support icon" />
+							</a>
 						</div>
 					</div>
 				</div>
-				<Login
-					isOpen={this.state.showLoginForm}
-					toggleLoginForm={this.toggleLoginForm}
-				/>
+			</div>
+			<Login
+				isOpen={this.state.showLoginForm}
+				toggleLoginForm={this.toggleLoginForm}
+			/>
 
-				<InitialSignup
-					isOpen={this.state.showInitialSignupForm}
-					toggleSignupForm={this.toggleSignupForm}
-					parent={this.props.parent}
-				/>
+			<InitialSignup
+				isOpen={this.state.showInitialSignupForm}
+				toggleSignupForm={this.toggleSignupForm}
+				parent={this.props.parent}
+			/>
 
-        <CommunitiesAndKit
-          isOpen={this.state.showCommunitiesAndKit}
-          toggleCommunitiesAndKit={this.toggleCommunitiesAndKit}
-        />
-      </div>
+			<CommunitiesAndKit
+				isOpen={this.state.showCommunitiesAndKit}
+				toggleCommunitiesAndKit={this.toggleCommunitiesAndKit}
+			/>
+		</div>
     );
   }
 }
