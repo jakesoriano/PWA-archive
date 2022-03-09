365d57bbc00437acaec739a51a181aac3464c7c0
Jomar Ocampo <amatz5s0ya@gmail.com>
2022-03-10 00:28:41 +0800

    Fix/staging (#197)
    * hide recaptcha
    
    * sort by code
    
    Co-authored-by: lance.malaga <lance.malaga@whitecloak.com>

--- src/helpers/locations.js
+++ src/helpers/locations.js
@@ -1,65 +1,75 @@
 import { data, countryCodes } from '_constant';
 
-export function getRegions () {
+export function getRegions() {
   try {
-    return data.map(i => {
+    return data.map((i) => {
       return {
         value: i.r,
-        text: i.r
-      }
+        text: i.r,
+      };
     });
   } catch (error) {
     return [];
   }
 }
 
-export function getProvince (r) {
+export function getProvince(r) {
   try {
-    return Object.keys(data.find(i => i.r === r).p).map(key => {
+    return Object.keys(data.find((i) => i.r === r).p).map((key) => {
       return {
         value: key,
-        text: key
-      }
+        text: key,
+      };
     });
   } catch (error) {
     return [];
   }
 }
 
-export function getMunicipality (r, pKey) {
+export function getMunicipality(r, pKey) {
   try {
-    return Object.keys(data.find(i => i.r === r).p[pKey].m).map(key => {
+    return Object.keys(data.find((i) => i.r === r).p[pKey].m).map((key) => {
       return {
         value: key,
-        text: key
-      }
+        text: key,
+      };
     });
   } catch (error) {
     return [];
   }
 }
 
-export function getBarangay (r, pKey, mKey) {
+export function getBarangay(r, pKey, mKey) {
   try {
-    return (data.find(i => i.r === r).p[pKey].m[mKey].b).map(key => {
-      return {
-        value: key,
-        text: key
-      }
-    });
+    return data
+      .find((i) => i.r === r)
+      .p[pKey].m[mKey].b.map((key) => {
+        return {
+          value: key,
+          text: key,
+        };
+      });
   } catch (error) {
     return [];
   }
 }
 
-export function getMobilePrefixOptions () {
+export function getMobilePrefixOptions() {
   try {
-    return countryCodes.map(i => {
-      return {
-        value: i.dial_code,
-        text: `${i.dial_code} (${i.code})`
-      }
-    });
+    console.error(countryCodes);
+    return countryCodes
+      .sort((a, b) => {
+        if (a.code < b.code) {
+          return -1;
+        }
+        return 1;
+      })
+      .map((i) => {
+        return {
+          value: i.dial_code,
+          text: `${i.dial_code} (${i.code})`,
+        };
+      });
   } catch (error) {
     return [];
   }
