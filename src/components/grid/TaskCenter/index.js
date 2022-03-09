6395650953b67796cc594e7a1101df1089cdf2cf
Jomar Ocampo <amatz5s0ya@gmail.com>
2022-03-09 18:33:12 +0800

    fix title and content spacing (#184)

--- src/components/grid/TaskCenter/index.js
+++ src/components/grid/TaskCenter/index.js
@@ -165,19 +165,6 @@ class TaskCenter extends Component {
 	          style={{ container: style.imgCont }}
 	        />
 	      </div>
-
-	      <div className={style.heartReact}>
-	        <p>
-						i{' - '}
-	          <svg className={style.heart} viewBox="0 0 32 29.6">
-	            <path
-	              d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
-	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"
-	            />
-	          </svg>{' '}
-						react
-	        </p>
-	      </div>
 	    </div>
 	  );
 	};
@@ -238,6 +225,15 @@ class TaskCenter extends Component {
 				</div> */}
 	          {/* Image */}
 	          <div className={style.taskBody}>
+	            {this.state.item?.title && (
+	              <p
+	                className={style.title}
+	                dangerouslySetInnerHTML={{
+	                  __html: this.state.item.title,
+	                }}
+	              />
+	            )}
+
 	            <a
 	              href={item.url}
 	              target="_blank"
