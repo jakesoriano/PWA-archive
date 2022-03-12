4debcacc12aab1f47b438945c38801f0925fb319
Lorenzo Valentino <lvalentino@unionbankph.com>
2022-03-12 12:55:21 +0800

    Updated Volunteer needs list

--- src/constant/volunteerNeeds.js
+++ src/constant/volunteerNeeds.js
@@ -1,10 +1,10 @@
 export const volunteerNeeds = [
-  { id: 'toda_stickering', value: 'TODA Stickering' },
   { id: 'tarpaulins', value: 'Tarpaulins' },
-	{ id: 'brgy_event', value: 'Barangay Event' },
-  { id: 'house_to_house_50', value: 'House to House Tarpaulin (50-100)' },
-	{ id: 'house_to_house_100', value: 'House to House Tarpaulin (100-200)' },
-	{ id: 'house_to_house_200', value: 'House to House Tarpaulin (200-400)' }
+	{ id: 'toda_stickering', value: 'TODA Stickering' },
+	{ id: 'brgy_event', value: 'Barangay Event' }
+  // { id: 'house_to_house_50', value: 'House to House Tarpaulin (50-100)' },
+	// { id: 'house_to_house_100', value: 'House to House Tarpaulin (100-200)' },
+	// { id: 'house_to_house_200', value: 'House to House Tarpaulin (200-400)' }
   // {id:'content', value: 'Content Writer'},
   // {id:'social', value: 'Social Media Content Posting'},
   // {id:'video', value: 'Video and Image Editors'},
